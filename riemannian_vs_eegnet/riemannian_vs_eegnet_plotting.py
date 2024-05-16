c602aa76c843ab9870e1220d5fc5ea5646d3c07c
ederm42 <eder.manuel19@gmail.com>
2024-05-16 03:34:11 +0200

    Adds scatter plotting for subject score pipelines comparisons

--- riemannian_vs_eegnet/riemannian_vs_eegnet_plotting.py
+++ riemannian_vs_eegnet/riemannian_vs_eegnet_plotting.py
@@ -40,6 +40,7 @@ def merge_results(path, filename_contains, filename_endswith=".csv", sep=";"):
         "WithinSession": "within-session",
         "CrossSession": "cross-session",
         "CrossSubject": "cross-subject",
+        "Tgsp": "TGSP",
     }, inplace=True, regex=True)
     return result_df
 
@@ -151,16 +152,19 @@ def plot_cnn_epochs_comparison(path, epoch_folders, ylim=None):
         # markers='^',
         hue_order=boxplot_order,
         alpha=0.1,
+        seed=444
     )
 
-    ax.legend(loc='lower right')
-    # ax.legend().set_visible(False)
+    #ax.legend(loc='lower right')
+    ax.legend(loc='upper left', bbox_to_anchor=(1.05, 1))
+    #ax.legend().set_visible(False)
 
     if ylim:
-        ax.set(yticks=np.arange(0.60, 0.851, 0.05))
+        ax.set(yticks=np.arange(0.7, 0.821, 0.02))
+
 
     # Adjust layout
-    plt.tight_layout()
+    #plt.tight_layout()
 
     # Show the plot
     plt.savefig(os.path.join(RESULTS_PATH, epoch_folders[-1], "plots", "epochs_comparison.jpg"), bbox_inches='tight', dpi=1200)
@@ -168,6 +172,67 @@ def plot_cnn_epochs_comparison(path, epoch_folders, ylim=None):
     plt.savefig(os.path.join(RESULTS_PATH, epoch_folders[-1], "plots", "epochs_comparison.eps"), bbox_inches='tight')
     plt.show()
 
+def plot_subject_scatter(path, paradigm_folders):
+    evaluations = ["WithinSession", "CrossSession", "CrossSubject"]
+
+    subject_scores_all = []
+
+    for paradigm_folder in paradigm_folders:
+        print(paradigm_folder)
+
+        result_dfs = []
+        for evaluation in evaluations:
+            try:
+                result_df = merge_results(os.path.join(path, paradigm_folder), filename_contains=evaluation)
+            except Exception as e:
+                print(e)
+                result_df = pd.DataFrame({})
+
+            result_dfs.append(result_df)
+
+        results_all = pd.concat(result_dfs, ignore_index=True)
+
+        print("A")
+        results_all["unique_subject_sessions"] = \
+            results_all["dataset"].astype(str) + "-" + \
+            results_all["subject"].astype(str) + "-" + \
+            results_all["session"].astype(str) + "-" + \
+            results_all["evaluation"].astype(str)
+
+        subject_scores_evaluation = []
+        for unique_subject_session in results_all["unique_subject_sessions"].unique():
+            results_all_filtered = results_all.loc[results_all["unique_subject_sessions"] == unique_subject_session]
+
+            eegnet_score = results_all_filtered.loc[results_all_filtered["pipeline"] == "EEGNet"]
+
+            tgsp_score = results_all_filtered[results_all_filtered["pipeline"].str.contains("TGSP")]["score"].max()
+            subject_scores = {
+                "unique_subject_session": unique_subject_session,
+                "EEGNet_score": eegnet_score["score"].values[0],
+                "best_TGSP_score": tgsp_score,
+                "evaluation": eegnet_score["evaluation"].values[0],
+                "dataset": eegnet_score["dataset"].values[0],
+                "paradigm": paradigm_folder
+            }
+            subject_scores_evaluation.append(subject_scores)
+
+        subject_scores_all.extend(subject_scores_evaluation)
+
+    subject_scores_df = pd.DataFrame(subject_scores_all)
+
+    # if problems with too many colors, move the plot creation inside the paradigm folder loop and create separately
+    for hue in ["evaluation", "dataset"]:
+        sns.lmplot(data=subject_scores_df,
+                   x="EEGNet_score", y="best_TGSP_score",
+                   hue=hue, col="paradigm")
+
+        plt.savefig(os.path.join(path, "plots", "subjects_score_scatter" + "_" + hue + ".jpg"), bbox_inches='tight', dpi=1200)
+        plt.savefig(os.path.join(path, "plots", "subjects_score_scatter" + "_" + hue + ".pdf"), bbox_inches='tight', format='pdf')
+        plt.savefig(os.path.join(path, "plots", "subjects_score_scatter" + "_" + hue + ".eps"), bbox_inches='tight', format='eps')
+        plt.show()
+    print("done")
 
 if __name__ == '__main__':
-    plot_cnn_epochs_comparison(path=os.path.join(os.getcwd(), "results"), epoch_folders=["100_epochs", "200_epochs", "300_epochs_final"], ylim=(0.6, 1.1))
+    plot_subject_scatter(path=os.path.join(os.getcwd(), "results", "300_epochs_final"), paradigm_folders=["MILR", "P300", "SSVEP"])
+    # plot_cnn_comparison(path=os.path.join(os.getcwd(), "results", "300_epochs_final"), paradigm_folders=["MILR", "P300", "SSVEP"], comparison_type="score", ylim=None)
+    # plot_cnn_epochs_comparison(path=os.path.join(os.getcwd(), "results"), epoch_folders=["100_epochs", "200_epochs", "300_epochs_final"], ylim=(0.6, 1.1))
